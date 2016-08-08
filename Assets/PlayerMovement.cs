63e7968cda002a6b33fc5e5418fe57b594a1ee2f
istateside <kevinrfleischman@gmail.com>
2016-08-07 22:30:31 -0400

    Adds basic player movement and (very) basic layout of main stae

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Boundary
+{
+  public float xMin, xMax, yMin, yMax;
+}
+
+public class PlayerMovement : MonoBehaviour {
+  public float speed;
+  public float tilt;
+  public Boundary boundary;
+  private Rigidbody rb;
+
+  void Start() {
+    rb = GetComponent<Rigidbody>();
+  }
+
+  void FixedUpdate() {
+    float moveHorizontal = Input.GetAxis("Horizontal");
+    float moveVertical = Input.GetAxis("Vertical");
+    Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0.0f);
+    rb.velocity = movement * speed;
+
+    rb.position = new Vector3(
+      Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
+      Mathf.Clamp(rb.position.y, boundary.yMin, boundary.yMax),
+      0.0f
+    );
+  }
+}
